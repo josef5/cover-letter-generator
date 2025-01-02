1a8b3e4e6df348b3d537142ed782ddb34e097c69
Jose Espejo <joseespejo@yahoo.com>
2025-01-02 22:12:58 +0000

    typescript: update fetchCompletion function to use UserData type

--- src/preload/preload.ts
+++ src/preload/preload.ts
@@ -1,8 +1,10 @@
 import { contextBridge, ipcRenderer } from "electron";
+import { UserData } from "../renderer/types/data";
 
 // API exposed to renderer
 contextBridge.exposeInMainWorld("api", {
-  fetchCompletion: (data: any) => ipcRenderer.invoke("fetch-completion", data), // TODO: replace any with type
+  fetchCompletion: (data: UserData) =>
+    ipcRenderer.invoke("fetch-completion", data),
   getStoreValue: (key: string) => ipcRenderer.invoke("getStoreValue", key),
   setStoreValue: (key: string, value: string) =>
     ipcRenderer.invoke("setStoreValue", key, value),
