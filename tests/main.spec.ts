8a5ed0af6fca0f868fc055b827cf28eaa094f7ba
Jose Espejo <joseespejo@yahoo.com>
2025-01-06 21:12:53 +0000

    test: add test for required inputs

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -1,14 +1,73 @@
 import {
   _electron as electron,
-  test,
   expect,
+  test,
   type ElectronApplication,
+  type ElementHandle,
   type Page,
 } from "@playwright/test";
 import { describe } from "node:test";
 
 let electronApp: ElectronApplication;
 let window: Page;
+let submitButton: ElementHandle<SVGElement | HTMLElement> | null;
+
+const inputFields = [
+  {
+    element: "input",
+    name: "salutation",
+    value: "Dear Hiring Manager,",
+    required: true,
+  },
+  {
+    element: "textarea",
+    name: "jobDescription",
+    value: "We are looking for a Software Engineer to join our team.",
+    required: true,
+  },
+  {
+    element: "textarea",
+    name: "additionalNotes",
+    value: "I am excited about this opportunity.",
+    required: false,
+  },
+  {
+    element: "input",
+    name: "settings.apiKey",
+    value: "abc123",
+    required: true,
+  },
+  {
+    element: "input",
+    name: "settings.name",
+    value: "John Doe",
+    required: true,
+  },
+  {
+    element: "input",
+    name: "settings.temperature",
+    value: "0.7",
+    required: true,
+    hasDefaultValue: true,
+  },
+  {
+    element: "input",
+    name: "settings.wordLimit",
+    value: "300",
+    required: true,
+  },
+  {
+    element: "textarea",
+    name: "settings.workExperience",
+    value: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    required: true,
+  },
+];
+
+// Filter out the required input fields that have a default value and are not emptyable
+const requiredInputFields = inputFields.filter(
+  ({ required, hasDefaultValue }) => required && !hasDefaultValue,
+);
 
 describe("Main", () => {
   test.beforeAll(async () => {
@@ -19,11 +78,8 @@ describe("Main", () => {
       args: ["main.js"],
     });
 
-    electronApp.on("console", (msg) => {
-      console.log(msg);
-    });
-
     window = await electronApp.firstWindow();
+    submitButton = await window.$('button[type="submit"]');
   });
 
   test.afterAll(async () => {
@@ -35,23 +91,12 @@ describe("Main", () => {
   });
 
   test("should not be submittable by default", async () => {
-    const submitButton = await window.$('button[type="submit"]');
     const isDisabled = await submitButton?.isDisabled();
 
     expect(isDisabled).toBe(true);
   });
 
-  async function fillOutForm() {
-    await window.fill('input[name="salutation"]', "Dear Hiring Manager,");
-    await window.fill(
-      'textarea[name="jobDescription"]',
-      "We are looking for a Software Engineer to join our team.",
-    );
-    await window.fill(
-      'textarea[name="additionalNotes"]',
-      "I am excited about this opportunity.",
-    );
-
+  async function openAccordion() {
     // Open the settings accordion
     const accordionTrigger = await window.$(
       '[data-testid="settings-accordion-trigger"]',
@@ -67,31 +112,49 @@ describe("Main", () => {
       // Set a delay to wait for the accordion to open
       await new Promise((resolve) => setTimeout(resolve, 1000));
     }
+  }
 
-    // Fill out settings in the accordion
-    await window.fill('input[name="settings.apiKey"]', "abc123");
-    await window.fill('input[name="settings.name"]', "John Doe");
+  async function fillOutForm(
+    values: { element: string; name: string; value: string }[],
+  ) {
+    await openAccordion();
 
+    // Set value for the model select
     await window.selectOption(
       "[data-testid='settings-model-select-trigger'] + select",
       "gpt-3.5-turbo",
     );
 
-    await window.fill('input[name="settings.temperature"]', "0.7");
-    await window.fill('input[name="settings.wordLimit"]', "300");
-    await window.fill(
-      'textarea[name="settings.workExperience"]',
-      "I have 4 years of experience in software development.",
-    );
+    // Set values for the input fields and textareas
+    for (const { element, name, value } of values) {
+      await window.fill(`${element}[name='${name}']`, value);
+    }
   }
 
   test("should be submittable when required fields are filled", async () => {
-    await fillOutForm();
+    await fillOutForm(inputFields);
 
-    const submitButton = await window.$('button[type="submit"]');
     const isDisabled = await submitButton?.isDisabled();
 
     expect(isDisabled).toBe(false);
+  });
+
+  test("should not be submittable when required fields are empty", async () => {
+    await openAccordion();
+
+    // Iterate over the required input fields
+    for (const { element, name, value } of requiredInputFields) {
+      const selector = `${element}[name='${name}']`;
+
+      // Clear the input field
+      await window.fill(selector, "");
+
+      // Expect the submit button to be disabled
+      expect(await submitButton?.isDisabled()).toBe(true);
+
+      // Refill the input field
+      await window.fill(selector, value);
+    }
   });
 
   // TODO: Figure out how to mock (intercept) api calls from the main process
