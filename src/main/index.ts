485cc1dfd9895bfdbb7bc2e4790561e3f74ae075
Jose Espejo <joseespejo@yahoo.com>
2025-02-11 21:35:01 +0000

    refactor: open DevTools in development

--- src/main/index.ts
+++ src/main/index.ts
@@ -31,8 +31,11 @@ function createWindow(): void {
 
   // HMR for renderer base on electron-vite cli.
   // Load the remote URL for development or the local html file for production.
-  if (is.dev && process.env["ELECTRON_RENDERER_URL"]) {
+  if (is.dev) {
+    if (process.env["ELECTRON_RENDERER_URL"]) {
     mainWindow.loadURL(process.env["ELECTRON_RENDERER_URL"]);
+    }
+    mainWindow.webContents.openDevTools();
   } else {
     mainWindow.loadFile(join(__dirname, "../renderer/index.html"));
   }
