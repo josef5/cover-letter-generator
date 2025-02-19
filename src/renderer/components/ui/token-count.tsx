18316e0c5ef968d14363e6fb6774530c32feeaa2
Jose Espejo <joseespejo@yahoo.com>
2025-02-19 21:52:02 +0000

    feat: enhance TokenCount component to accept custom className prop

--- src/renderer/components/ui/token-count.tsx
+++ src/renderer/components/ui/token-count.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-function TokenCount({ children }: { children: React.ReactNode }) {
-  return <div className="pr-2 text-right text-xs">{children}</div>;
+function TokenCount({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <div className={`pr-2 text-right text-xs ${className}}`}>{children}</div>
+  );
 }
 
 export default TokenCount;
